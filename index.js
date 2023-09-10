002a9557df204f2b7524d89a3281e1de6759293d
nogist <nogistune.me@gmail.com>
2023-09-10 21:55:24 +0100

    editing endpoin url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/api/info", (req, res) => {
+app.get("/api", (req, res) => {
   const slack_name = req.query.slack_name || "";
   const track = req.query.track || "";
 
